01ccb96fb65bd7d58f6d7883e1d6e67377fedb19
Thomas Klepzig <thomas.klepzig@comparex.de>
2017-02-02 15:43:16 +0100

    dummy daten funzt

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController()
+        {
+        }
         public IActionResult Index()
         {
             var buecher = new List<Buch>();
@@ -20,12 +23,16 @@ namespace WebApplication.Controllers
 
         public IActionResult Log()
         {
-            return Json("blubb");
+            var log = new List<BuchLog>();
+            log.Add(new BuchLog { Ereignis = "Angelegt", Datum = DateTime.Now, Daten = "Iwas" });
+            log.Add(new BuchLog { Ereignis = "Verliehen", Datum = DateTime.Now.AddDays(1), Daten = "Daniel" });
+            return Json(log);
         }
 
         [HttpPost]
         public IActionResult New(string titel)
         {
+            //new
             return RedirectToAction("Index");
         }
 
