248573706197f48bf57b73f0f4ebe5558be0fda1
Alexander Tank <Alexander.Tank@comparex.de>
2017-02-02 15:35:29 +0100

    Stuff

---
+++ src/EventStorage/EventStore.cs
@@ -0,0 +1,51 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace WebApplication.EventStorage
+{
+    public class EventStore
+    {
+        public void EventHinzufügen(AngelegtEvent @event)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BuchEvent[] LadeAlleEvents()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class EventStoreTests
+    {
+        [Fact]
+        public void Events_werden_hinzugefügt()
+        {
+            // Arrange
+            var eventStore = new EventStore();
+            var @event = new AngelegtEvent();
+
+            // Act
+            eventStore.EventHinzufügen(@event);
+
+            // Assert
+            eventStore.LadeAlleEvents().ShouldContain(@event);
+        }
+
+        public void Sollte_alle_Events_zurückgeben()
+        {
+            // Arrange
+            var eventStore = new EventStore();
+            var @event = new AngelegtEvent();
+            eventStore.EventHinzufügen(@event);
+            eventStore.EventHinzufügen(@event);
+
+            // Act
+            var events = eventStore.LadeAlleEvents();
+
+            // Assert
+            events.ShouldNotBeNull();
+            events.Length.ShouldBe(2);
+        }
+    }
+}
