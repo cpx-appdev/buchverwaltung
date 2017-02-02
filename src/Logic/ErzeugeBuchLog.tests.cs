4ffc141c3e713b2b96346ef97af3064574360817
Andreas Gräfe <andreas.graefe@comparex.de>
2017-02-02 15:22:33 +0100

    Tests für ErzeugeBuchlog hinzugefügt

--- src/Logic/ErzeugeBuchLog.tests.cs
+++ src/Logic/ErzeugeBuchLog.tests.cs
@@ -1,27 +1,31 @@
+using Shouldly;
 using System;
 using System.Collections.Generic;
+using Xunit;
 
 public class ErzeugeBuchLogTest
 {
 
-
+    [Fact]
     public void Validiere_Mapping_AngelegtEvent()
     {
         // Arange
         var buchLogHandler = new ErzeugeBuchLog();
         var buchEvents = new List<AngelegtEvent>();
 
-        buchEvents.Add(new AngelegtEvent() { Titel = "VB6", Datum = DateTime.Parse("2017-02-01"), EventId =  new Guid("7c9e6679-7425-40de-944b-e07fc1f90ae7")  } );
+        buchEvents.Add(new AngelegtEvent() {
+            Titel = "VB6",
+            Datum = DateTime.Parse("2017-02-01"),
+            EventId =  new Guid("7c9e6679-7425-40de-944b-e07fc1f90ae7")  } );
 
         // Act
         var result = buchLogHandler.Handle(buchEvents.ToArray());
 
         // Assert
-
-        //result.
-
-
-
+        result.Item1.ShouldBe("VB6");
+        result.Item2.ShouldContain(new BuchLog() { Daten = "VB6", Datum = DateTime.Parse("2017-02-01"), Ereignis = "Angelegt" });
+        result.Item2.Length.ShouldBe(1);
     }
+
 
 }
