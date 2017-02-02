f8798aecf29b926c968550cd9d11f4b2270062e6
Thomas Klepzig <thomas.klepzig@comparex.de>
2017-02-02 15:23:21 +0100

    Vorbereitung New und Log

--- src/Controllers/HomeController.cs
+++ src/Controllers/HomeController.cs
@@ -11,12 +11,24 @@ namespace WebApplication.Controllers
         public IActionResult Index()
         {
             var buecher = new List<Buch>();
-            buecher.Add(new Buch { Titel = "Buch 1", VerliehenAm = DateTime.Now.AddDays(-2), VerliehenAn = "Markus" });
-            buecher.Add(new Buch { Titel = "Buch 2", VerliehenAm = DateTime.Now.AddDays(-7), VerliehenAn = "Jens" });
+            buecher.Add(new Buch { Id = Guid.NewGuid(), Titel = "Buch 1", VerliehenAm = DateTime.Now.AddDays(-2), VerliehenAn = "Markus" });
+            buecher.Add(new Buch { Id = Guid.NewGuid(), Titel = "Buch 2", VerliehenAm = DateTime.Now.AddDays(-7), VerliehenAn = "Jens" });
 
             var viewModel = buecher.Select(x => new BuchViewModel(x)).ToList();
             return View(viewModel);
         }
+
+        public IActionResult Log()
+        {
+            return Json("blubb");
+        }
+
+        [HttpPost]
+        public IActionResult New(string titel)
+        {
+            return RedirectToAction("Index");
+        }
+
 
         public IActionResult Error()
         {
--- src/ViewModels/BuchViewModel.cs
+++ src/ViewModels/BuchViewModel.cs
@@ -1,13 +1,17 @@
+using System;
+
 public class BuchViewModel
     {
         public string Titel { get; set; }
         public string VerliehenAn { get; set; }
         public string VerliehenAm { get; set; }
+        public Guid Id { get; set; }
 
         public BuchViewModel(Buch buch)
         {
             this.Titel = buch.Titel;
             this.VerliehenAm = buch.VerliehenAm.ToString("yyyy.MM.dd, HH:mm:ss");
             this.VerliehenAn = buch.VerliehenAn;
+            this.Id = buch.Id;
         }
     }
