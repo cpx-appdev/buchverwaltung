93e6b7385072ac538ed60433285e4bc7a2a5808e
Jana Hehr <jana.hehr@comparex.de>
2017-02-02 15:10:54 +0100

    Anlegen Interaktor

--- src/Interaktor/BuchverwaltungInteraktor.cs
+++ src/Interaktor/BuchverwaltungInteraktor.cs
@@ -8,18 +8,27 @@ public class BuchverwaltungInteraktor
 {
     private IEventStorage _eventstorage;
     private IErzeugeBuchliste _erzeugeBuchliste;
+    private IErzeugeAngelegtEvent _erzeugeAngelegtEvent;
 
     public BuchverwaltungInteraktor(
         IEventStorage eventStorage,
-        IErzeugeBuchliste erzeugeBuchliste)
+        IErzeugeBuchliste erzeugeBuchliste,
+        IErzeugeAngelegtEvent erzeugeAngelegtEvent)
     {
         _eventstorage = eventStorage;
         _erzeugeBuchliste = erzeugeBuchliste;
+        _erzeugeAngelegtEvent = erzeugeAngelegtEvent;
     }
 
     public IList<Buch> Start()
     {
         var buchEvents = _eventstorage.LadeAlleEvents();
         return _erzeugeBuchliste.Handle(buchEvents);
+    }
+
+    public void BuchAnlegen(string titel)
+    {
+        var angelegtEvent = _erzeugeAngelegtEvent.Handle(titel);
+        _eventstorage.EventHinzufügen(angelegtEvent);
     }
 }
